0f420dfd2df9c56fdd7f9f15b6d0d531ce8b1b20
mipadrastroesmalo <mipadrastroesmalo@gmail.com>
2024-09-19 11:16:36 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "./assets/img/Cristiano-Ronaldopng.parspng.com-4.png"
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <img src={miLogo} alt= "The GOAT !" />
+      <h1>Cristiano Ronaldo  </h1>
+      <p>Proyecto CR7 en React</p>
+     </div>
+    </>
+  )
+}
+
+export default App
